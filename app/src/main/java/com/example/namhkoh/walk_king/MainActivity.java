2848b11def10f7b2532673c902c7b65a4250cd05
Nam Ho Koh <lucaskoh.lk@gmail.com>
2020-07-17 20:07:57 -0700

    Implementing step counter.

--- app/src/main/java/com/example/namhkoh/walk_king/MainActivity.java
+++ app/src/main/java/com/example/namhkoh/walk_king/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.namhkoh.walk_king;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sensorManager;
+
+    TextView stepValue;
+
+    boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        stepValue = (TextView) findViewById(R.id.StepValue);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this,countSensor,SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this,"Sensor not found!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
+        // if you unregister the hardware will stop detecting steps.
+        // sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
